c7f95d4ee2d4f3950fe15ebea5d95c1addce3855
Tim Hallyburton <tim.hallyburton@gmx.de>
2017-11-23 15:47:23 +0100

    Fixing project setup

---
+++ src/jalien/ContextualLogManager.java
@@ -0,0 +1,14 @@
+/**
+ * ContextualLogManager.java
+ * @author thallybu
+ * @since Nov 23, 2017
+ */
+package jalien;
+
+/**
+ * @author thallybu
+ *
+ */
+public class ContextualLogManager extends LogManager {
+
+}
