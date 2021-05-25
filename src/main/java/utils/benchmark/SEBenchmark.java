d8f369db8ba16c0517c2efe7660a3b51af148b21
Costin Grigoras <costing@gmail.com>
2021-05-25 17:21:03 +0200

    Options to read back files and to keep them on disk

--- src/main/java/utils/benchmark/SEBenchmark.java
+++ src/main/java/utils/benchmark/SEBenchmark.java
@@ -8,6 +8,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -41,15 +43,29 @@ public class SEBenchmark {
 
 	private static final AtomicLong uploadedSoFar = new AtomicLong();
 
+	private static final AtomicLong downloadedSoFar = new AtomicLong();
+
 	private static final AtomicInteger completed = new AtomicInteger();
 
 	private static final AtomicInteger failed = new AtomicInteger();
+
+	private static final AtomicInteger downloadedOk = new AtomicInteger();
+
+	private static final AtomicInteger downloadedErr = new AtomicInteger();
 
 	private static long startup;
 
 	private static final Object lock = new Object();
 
 	private static String fileNamePrefix = ConfigUtils.getLocalHostname();
+
+	private static LinkedBlockingQueue<String> readBackFiles = null;
+
+	private static int readBackThreads = 0;
+
+	private static boolean keepWrittenFiles = false;
+
+	private static volatile boolean shutdown = false;
 
 	private static final Thread monitoringThread = new Thread() {
 		{
@@ -72,13 +88,25 @@ public class SEBenchmark {
 
 				final double rate = (uploadedSoFar.longValue() * 1000. / dTime) / 1024 / 1024;
 
-				System.err.println("So far " + completed + " files (" + Format.size(uploadedSoFar.longValue()) + ") have completed in " + Format.toInterval(dTime) + ", for an average rate of "
+				System.err.print("So far " + completed + " files (" + Format.size(uploadedSoFar.longValue()) + ") have completed in " + Format.toInterval(dTime) + ", for an average rate of "
 						+ (Format.point(rate) + " MB/s") + "; " + failed + " failures");
+
+				if (downloadedSoFar.get() > 0) {
+					final double dRate = downloadedSoFar.longValue() * 1000. / dTime / 1024 / 1024;
+
+					System.err.println(
+							", download stats: " + downloadedOk + " ok (" + Format.size(downloadedSoFar.longValue()) + "), " + downloadedErr + " errors, dw rate " + Format.point(dRate) + " MB/s");
+				}
+
+				System.err.println();
 			}
 		}
 	};
 
 	private static final boolean cleanupCatalogueFile(final String fileName) {
+		if (fileName == null)
+			return true;
+
 		try {
 			final JAliEnCOMMander cmd = JAliEnCOMMander.getInstance();
 
@@ -98,6 +126,51 @@ public class SEBenchmark {
 		}
 	}
 
+	private static final class ReadThread extends Thread {
+		@Override
+		public void run() {
+			final JAliEnCOMMander commander = JAliEnCOMMander.getInstance();
+
+			while (true) {
+				try {
+					final String s = readBackFiles.take();
+
+					if (s.isBlank())
+						break;
+
+					final File fTemp = File.createTempFile("SEBenchmark", "download");
+
+					fTemp.delete();
+
+					try (Timing t = new Timing()) {
+						if (commander.c_api.downloadFile(s, fTemp) != null) {
+							System.err.println("Downloaded " + s + " in " + t);
+							downloadedSoFar.addAndGet(fTemp.length());
+							downloadedOk.incrementAndGet();
+						}
+						else {
+							System.err.println("Failed to download " + s);
+							downloadedErr.incrementAndGet();
+						}
+					}
+					finally {
+						fTemp.delete();
+
+						if (shutdown || (!readBackFiles.offer(s) && !keepWrittenFiles))
+							cleanupCatalogueFile(s);
+					}
+				}
+				catch (@SuppressWarnings("unused") final InterruptedException ie) {
+					break;
+				}
+				catch (final IOException ioe) {
+					downloadedErr.incrementAndGet();
+					System.err.println("Exception downloading a file: " + ioe.getMessage());
+				}
+			}
+		}
+	}
+
 	private static final class UploadThread extends Thread {
 		private int iterations;
 		private final int tNo;
@@ -113,16 +186,24 @@ public class SEBenchmark {
 
 		@Override
 		public void run() {
-			final String testPath = UsersHelper.getHomeDir(account.getDefaultUser()) + "se_test_" + fileNamePrefix + "_" + tNo;
+			final String basePath = UsersHelper.getHomeDir(account.getDefaultUser()) + "se_test_" + fileNamePrefix + "/" + tNo;
 
-			System.err.println("Using this catalogue file name: " + testPath);
+			int count = 0;
+
+			JAliEnCOMMander.getInstance().c_api.createCatalogueDirectory(basePath, true);
+
+			String testPath = null;
 
 			do {
+				if (testPath == null) {
+					testPath = basePath + "/" + (++count) + "." + UUID.randomUUID().toString();
+				}
+
 				if (!cleanupCatalogueFile(testPath)) {
 					try {
 						sleep(1000);
 					}
-					catch (@SuppressWarnings("unused") InterruptedException e) {
+					catch (@SuppressWarnings("unused") final InterruptedException e) {
 						break;
 					}
 
@@ -130,13 +211,26 @@ public class SEBenchmark {
 				}
 
 				try (Timing timing = new Timing()) {
-					final LFN target = IOUtils.upload(localFile, testPath, account, null, "-S", seName);
+					final LFN target = IOUtils.upload(localFile, testPath, account, System.err, "-S", seName);
 
 					if (target != null) {
-						System.err.println("Thread " + tNo + " completed one upload in " + timing + " (" + Format.point(localFile.length() / timing.getSeconds() / 1024 / 1024) + " MB/s)");
+						System.err.println("Thread " + tNo + " completed one upload in " + timing + " (" + Format.point(localFile.length() / timing.getSeconds() / 1024 / 1024) + " MB/s): "
+								+ target.getCanonicalName());
 
 						uploadedSoFar.addAndGet(localFile.length());
 						completed.incrementAndGet();
+
+						if (readBackThreads > 0) {
+							if (!readBackFiles.offer(testPath) && !keepWrittenFiles) {
+								System.err.println("Removing just uploaded file " + testPath);
+								cleanupCatalogueFile(testPath);
+							}
+
+							testPath = null;
+						}
+
+						if (keepWrittenFiles)
+							testPath = null;
 					}
 					else {
 						System.err.println("Failed to upload to " + testPath);
@@ -162,6 +256,9 @@ public class SEBenchmark {
 	 * @throws IOException
 	 */
 	public static void main(final String[] args) throws IOException {
+		// System.setProperty("alien.io.protocols.TempFileManager.persistent.entries", "0");
+		// System.setProperty("alien.io.protocols.TempFileManager.temp.entries", "0");
+
 		final OptionParser parser = new OptionParser();
 
 		parser.accepts("s").withRequiredArg();
@@ -170,6 +267,8 @@ public class SEBenchmark {
 		parser.accepts("f").withRequiredArg();
 		parser.accepts("i").withRequiredArg().ofType(Integer.class);
 		parser.accepts("n").withRequiredArg();
+		parser.accepts("r").withRequiredArg().ofType(Integer.class);
+		parser.accepts("k");
 
 		final OptionSet options = parser.parse(args);
 
@@ -181,6 +280,8 @@ public class SEBenchmark {
 			System.err.println("\t-b <file size, in MB>\t\t(optional, default " + DEFAULT_SIZE + ")");
 			System.err.println("\t-f <file name>\t\t\t(optional, file to use, size to be extracted from it)");
 			System.err.println("\t-n <catalogue name>\t\t(optional, default '" + fileNamePrefix + "')");
+			System.err.println("\t-r <threads>\t\t(number of read back threads, optional, default 0)");
+			System.err.println("\t-k\t\t(pass this flag to keep written files on disk)");
 
 			return;
 		}
@@ -227,6 +328,8 @@ public class SEBenchmark {
 		if (options.has("n"))
 			fileNamePrefix = (String) options.valueOf("n");
 
+		keepWrittenFiles = options.has("k");
+
 		final List<UploadThread> tList = new ArrayList<>(threads);
 
 		startup = System.currentTimeMillis();
@@ -239,6 +342,20 @@ public class SEBenchmark {
 			tList.add(ut);
 		}
 
+		final List<ReadThread> rList = new ArrayList<>();
+
+		if (options.has("r")) {
+			readBackThreads = ((Integer) options.valueOf("r")).intValue();
+
+			for (int i = 0; i < readBackThreads; i++) {
+				final ReadThread rt = new ReadThread();
+				rt.start();
+				rList.add(rt);
+			}
+		}
+
+		readBackFiles = new LinkedBlockingQueue<>(readBackThreads);
+
 		monitoringThread.start();
 
 		for (final UploadThread ut : tList)
@@ -248,6 +365,25 @@ public class SEBenchmark {
 			catch (@SuppressWarnings("unused") final InterruptedException e) {
 				// ignore
 			}
+
+		shutdown = true;
+
+		for (int i = 0; i < readBackThreads; i++)
+			try {
+				readBackFiles.put("");
+			}
+			catch (@SuppressWarnings("unused") final InterruptedException e) {
+				// ignore
+			}
+
+		for (final ReadThread rt : rList) {
+			try {
+				rt.join();
+			}
+			catch (@SuppressWarnings("unused") final InterruptedException ie) {
+				// ignore
+			}
+		}
 
 		synchronized (lock) {
 			lock.notifyAll();
