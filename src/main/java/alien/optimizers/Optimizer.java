4390de2df39cedcec4674ec48a660615678a0c7b
Iuliana BRÎNZOI (87571) <iuliana.brinzoi@stud.acs.upb.ro>
2021-06-24 11:02:36 +0300

    Added the LFN Crawler to the optimizers list so that it would be started automatically

--- src/main/java/alien/optimizers/Optimizer.java
+++ src/main/java/alien/optimizers/Optimizer.java
@@ -18,7 +18,7 @@ public class Optimizer extends Thread {
 
 	private long sleep_period = 60 * 1000L; // 1min
 
-	private static String[] catalogue_optimizers = { "alien.optimizers.catalogue.LTables", "alien.optimizers.catalogue.GuidTable", "alien.optimizers.catalogue.ResyncLDAP" };
+	private static String[] catalogue_optimizers = { "alien.optimizers.catalogue.LTables", "alien.optimizers.catalogue.GuidTable", "alien.optimizers.catalogue.ResyncLDAP", "utils.lfncrawler.LFNCrawler" };
 
 	@Override
 	public void run() {
