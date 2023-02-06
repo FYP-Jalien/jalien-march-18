27e5e56a3b2ffde2b728434ea8d29e4673aec02e
Max <mstoretv@cern.ch>
2023-02-06 11:24:45 +0100

    Reduce wait delay between failed jobs

--- src/main/java/alien/site/JobAgent.java
+++ src/main/java/alien/site/JobAgent.java
@@ -490,7 +490,7 @@ public class JobAgent implements Runnable {
 		// Wait before matching if previous jobs have been failing
 		if (attempts.getAcquire() > 1) {
 			try {
-				final int timeToWait = (int) Math.pow(5, attempts.getAcquire());
+				final int timeToWait = (int) Math.pow(2, attempts.getAcquire());
 				logger.log(Level.INFO, "A previous job failed. Will wait " + timeToWait + "s before continuing...");
 				Thread.sleep(timeToWait * 1000);
 			}
