0f94243c218e17017285310b253c2565f458f301
Max <mstoretv@cern.ch>
2023-06-12 10:55:57 +0200

    Do a sanity check before matching job

--- src/main/java/alien/site/JobAgent.java
+++ src/main/java/alien/site/JobAgent.java
@@ -527,10 +527,16 @@ public class JobAgent implements Runnable {
 					logger.log(Level.WARNING, "Unable to check for AVX support", ex);
 				}
 
+				// Verify environment if there are no containers, before matching
 				if (containerizer == null) {
 					if (env.getOrDefault("ALIENV_ERRORS", "").contains("TRUE") || env.getOrDefault("XRDCP_ERRORS", "").contains("TRUE")) {
 						logger.log(Level.SEVERE, "The environment on this node appears to be broken. Please do \"" + CVMFS.getAlienvPrint() + "\" for more debug info.");
 						throw new EOFException("Job matching aborted due to potentially misconfigured environment");
+					}
+					CommandOutput sanityCheck = SystemCommand.bash("ps --version");
+					if (!sanityCheck.stdout.contains("from")) {
+						logger.log(Level.SEVERE, "Possibly broken environment or process limit reached: " + sanityCheck.stderr);
+						throw new EOFException("Job matching aborted due to failed sanity check");
 					}
 				}
 
