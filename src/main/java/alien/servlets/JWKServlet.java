ae51ed2bca78f1a0f6c42af9eca898eed37e2a1a
vyurchen <volodymyr.yurchenko@cern.ch>
2024-02-08 14:47:39 +0100

    Replace test public key with the production default public key

--- src/main/java/alien/servlets/JWKServlet.java
+++ src/main/java/alien/servlets/JWKServlet.java
@@ -1,12 +1,15 @@
 package alien.servlets;
 
+import alien.config.ConfigUtils;
 import alien.user.JAKeyStore;
+import alien.user.UserFactory;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Objects;
@@ -16,13 +19,18 @@ public class JWKServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-           String jsonData = "{\"keys\":[{\n" +
+        String JAuthZPubLocation = ConfigUtils.getConfig().gets("jAuthZ.pub.key.location", UserFactory.getUserHome() +
+                FileSystems.getDefault().getSeparator() + ".alien" +
+                FileSystems.getDefault().getSeparator() + "authen" +
+                FileSystems.getDefault().getSeparator() + "lpub.pem");
+
+        String jsonData = "{\"keys\":[{\n" +
                 "    \"kty\": \"RSA\",\n" +
                 "    \"use\": \"sig\",\n" +
                 "    \"e\": \"AQAB\",\n" +
                 "    \"kid\": \"ALICE\",\n" +
                 "    \"n\": \"" +
-                Base64.getUrlEncoder().encodeToString(Arrays.toString(Objects.requireNonNull(JAKeyStore.loadPubX509("/etc/grid-security/tokencert.pem", false))).getBytes()) + "\",\n" +
+                Base64.getUrlEncoder().encodeToString(Arrays.toString(Objects.requireNonNull(JAKeyStore.loadPubX509(JAuthZPubLocation, false))).getBytes()) + "\",\n" +
                 "    \"alg\": \"RS256\"\n" +
                 "}]}";
 
