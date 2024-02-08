e3ca24132c9fd66ec73113b4de8a51565723ac7e
Sergiu Weisz <sergiu121@gmail.com>
2024-02-08 19:12:03 +0200

    Get BATCH_ARGS variable from env in SFAPI

--- src/main/java/alien/site/batchqueue/SFAPI.java
+++ src/main/java/alien/site/batchqueue/SFAPI.java
@@ -89,6 +89,8 @@ public class SFAPI extends BatchQueue {
 			}
 		}
 
+		this.clientId = environment.getOrDefault("BATCH_ARGS", clientId);
+		this.clientId = environment.getOrDefault("RUN_ARGS", clientId);
 		this.clientId = environment.getOrDefault("CLIENT_ID", clientId);
 	}
 
