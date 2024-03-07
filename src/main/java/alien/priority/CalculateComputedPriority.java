d38a79e3c47fb53390b98c881ead5b32ed4ce190
Jorn-Are Klubben Flaten <jorn-are.klubben.flaten@cern.ch>
2024-03-07 13:15:26 +0100

    updating computedPriority row by row instead of as a bulk update

--- src/main/java/alien/priority/CalculateComputedPriority.java
+++ src/main/java/alien/priority/CalculateComputedPriority.java
@@ -20,118 +20,112 @@ import java.util.logging.Logger;
  * @since 2023-12-04
  */
 public class CalculateComputedPriority {
-	/**
-	 * Logger
-	 */
-	static final Logger logger = ConfigUtils.getLogger(PriorityRapidUpdater.class.getCanonicalName());
+    /**
+     * Logger
+     */
+    static final Logger logger = ConfigUtils.getLogger(PriorityRapidUpdater.class.getCanonicalName());
 
-	/**
-	 * Monitoring component
-	 */
-	static final Monitor monitor = MonitorFactory.getMonitor(PriorityRapidUpdater.class.getCanonicalName());
+    /**
+     * Monitoring component
+     */
+    static final Monitor monitor = MonitorFactory.getMonitor(PriorityRapidUpdater.class.getCanonicalName());
 
-	/**
-	 * 
-	 */
-	public static void updateComputedPriority(boolean onlyActiveUsers) {
-		StringBuilder registerLog = new StringBuilder();
-		try (DBFunctions db = TaskQueueUtils.getQueueDB(); DBFunctions dbdev = TaskQueueUtils.getProcessesDevDB()) {
-			if (db == null) {
-				logger.log(Level.SEVERE, "CalculatePriority could not get a DB connection");
-				return;
-			}
+    /**
+     * Update the computed priority for users
+     */
+    public static void updateComputedPriority(boolean onlyActiveUsers) {
+        StringBuilder registerLog = new StringBuilder();
+        try (DBFunctions db = TaskQueueUtils.getQueueDB(); DBFunctions dbdev = TaskQueueUtils.getProcessesDevDB()) {
+            if (db == null) {
+                logger.log(Level.SEVERE, "CalculatePriority could not get a DB connection");
+                return;
+            }
 
-			if (dbdev == null) {
-				logger.log(Level.SEVERE, "CalculatePriority(processesDev) could not get a DB connection");
-				return;
-			}
+            if (dbdev == null) {
+                logger.log(Level.SEVERE, "CalculatePriority(processesDev) could not get a DB connection");
+                return;
+            }
 
-			db.setQueryTimeout(60);
-			dbdev.setQueryTimeout(60);
+            db.setQueryTimeout(60);
+            dbdev.setQueryTimeout(60);
 
-			String q;
-			if(onlyActiveUsers) {
-				q = "SELECT userId, priority, running, maxParallelJobs, totalRunningTimeLast24h, maxTotalRunningTime from PRIORITY where totalRunningTimeLast24h > 0";
-			} else {
-				q = "SELECT userId, priority, running, maxParallelJobs, totalRunningTimeLast24h, maxTotalRunningTime from PRIORITY";
-			}
+            String q;
+            if (onlyActiveUsers) {
+                q = "SELECT userId, priority, running, maxParallelJobs, totalRunningTimeLast24h, maxTotalRunningTime from PRIORITY where totalRunningTimeLast24h > 0";
+            } else {
+                q = "SELECT userId, priority, running, maxParallelJobs, totalRunningTimeLast24h, maxTotalRunningTime from PRIORITY";
+            }
 
-			Map<Integer, PriorityDto> dtos = new HashMap<>();
-			try (Timing t = new Timing(monitor, "calculateComputedPriority")) {
-				logger.log(Level.INFO, "Calculating computed priority");
-				db.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
-				db.query(q);
+            Map<Integer, PriorityDto> dtos = new HashMap<>();
+            try (Timing t = new Timing(monitor, "calculateComputedPriority")) {
+                logger.log(Level.INFO, "Calculating computed priority");
+                db.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
+                db.query(q);
 
-				while (db.moveNext()) {
-					Integer userId = Integer.valueOf(db.geti("userId"));
-					dtos.computeIfAbsent(
-							userId,
-							k -> new PriorityDto(db));
+                while (db.moveNext()) {
+                    Integer userId = Integer.valueOf(db.geti("userId"));
+                    dtos.computeIfAbsent(
+                            userId,
+                            k -> new PriorityDto(db));
 
-					updateComputedPriority(dtos.get(userId));
-				}
-				registerLog.append("Calculating computed priority for ")
-						.append(dtos.size())
-						.append(" users.\n")
-						.append(" Updated userload and computedPriority values written to PRIORITY table in processesdev DB.\n ");
+                    updateComputedPriority(dtos.get(userId));
+                }
+                registerLog.append("Calculating computed priority for ")
+                        .append(dtos.size())
+                        .append(" users.\n")
+                        .append(" Updated userload and computedPriority values will be written to the PRIORITY table in processesdev DB.\n ");
 
-				logger.log(Level.INFO, "Finished calculating, updating PRIORITY table");
-				StringBuilder sb = new StringBuilder("INSERT INTO PRIORITY (userId, userload, computedPriority) VALUES ");
-				logger.log(Level.INFO, "Elements in DTO map: " + dtos.size());
+                logger.log(Level.INFO, "Finished calculating, preparing to update " + dtos.size() + " elements in the PRIORITY table...");
+                executeUpdateQuery(dbdev, dtos, registerLog);
 
-				boolean first = true;
-				for (PriorityDto dto : dtos.values()) {
-					if (!first) {
-						sb.append(", ");
-					}
-					else {
-						first = false;
-					}
+                DBSyncUtils.registerLog(CalculateComputedPriority.class.getCanonicalName(), registerLog.toString());
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Exception thrown while calculating computedPriority", e);
+            }
+        }
+    }
 
-					sb.append("(")
-							.append(dto.getUserId()).append(", ")
-							.append(dto.getUserload()).append(", ")
-							.append(dto.getComputedPriority())
-							.append(")");
-				}
+    private static void executeUpdateQuery(DBFunctions dbdev, Map<Integer, PriorityDto> dtos, StringBuilder registerLog) {
+        try (Timing t = new Timing(monitor, "TQ_update_computed_priority_ms")) {
+            t.startTiming();
+            dtos.forEach((id, dto) -> {
+                Timing t2 = new Timing(monitor, "TQ_single_row_update_ms");
+                String query = updateUserQuery(id, dto);
+                dbdev.query(query);
+                t2.endTiming();
+                logger.log(Level.INFO, "Updating PRIORITY row for user " + id + " completed in " + t2.getMillis() + " ms");
+            });
 
-				sb.append(" ON DUPLICATE KEY UPDATE ")
-						.append("userload = VALUES(userload), ")
-						.append("computedPriority = VALUES(computedPriority)");
+            t.endTiming();
+            logger.log(Level.INFO, "Finished updating PRIORITY table row by row, took " + t.getMillis() + " ms");
+            registerLog.append("Updating PRIORITY table row by row completed in ").append(t.getMillis()).append(" ms\n");
+        }
+    }
 
-				dbdev.query(sb.toString());
-				t.endTiming();
-				logger.log(Level.INFO, "Finished updating PRIORITY table, took " + t.getMillis() + " ms");
+    private static String updateUserQuery(Integer userId, PriorityDto dto) {
+        return "UPDATE PRIORITY SET userload = " + dto.getUserload()
+                + ", computedPriority = " + dto.getComputedPriority()
+                + " WHERE userId = " + userId;
+    }
 
-				registerLog.append("Procedure finished in ").append(t.getMillis()).append(" ms");
-				DBSyncUtils.registerLog(CalculateComputedPriority.class.getCanonicalName(), registerLog.toString());
-			}
-			catch (Exception e) {
-				logger.log(Level.SEVERE, "Exception thrown while calculating computedPriority", e);
-			}
-		}
-	}
+    private static void updateComputedPriority(PriorityDto dto) {
+        int activeCpuCores = dto.getRunning();
+        int maxCpuCores = dto.getMaxParallelJobs();
+        long historicalUsage = dto.getTotalRunningTimeLast24h() / dto.getMaxTotalRunningTime();
 
-	private static void updateComputedPriority(PriorityDto dto) {
-		int activeCpuCores = dto.getRunning();
-		int maxCpuCores = dto.getMaxParallelJobs();
-		long historicalUsage = dto.getTotalRunningTimeLast24h() / dto.getMaxTotalRunningTime();
+        if (activeCpuCores < maxCpuCores) {
+            double coreUsageCost = activeCpuCores == 0 ? 1 : (activeCpuCores * Math.exp(-historicalUsage));
+            float userLoad = (float) activeCpuCores / maxCpuCores;
+            dto.setUserload(userLoad);
+            double adjustedPriorityFactor = (2.0 - userLoad) * (dto.getPriority() / coreUsageCost);
 
-		if (activeCpuCores < maxCpuCores) {
-			double coreUsageCost = activeCpuCores == 0 ? 1 : (activeCpuCores * Math.exp(-historicalUsage));
-			float userLoad = (float) activeCpuCores / maxCpuCores;
-			dto.setUserload(userLoad);
-			double adjustedPriorityFactor = (2.0 - userLoad) * (dto.getPriority() / coreUsageCost);
-
-			if (adjustedPriorityFactor > 0) {
-				dto.setComputedPriority((float) (50.0 * adjustedPriorityFactor));
-			}
-			else {
-				dto.setComputedPriority(1);
-			}
-		}
-		else {
-			dto.setComputedPriority(1);
-		}
-	}
+            if (adjustedPriorityFactor > 0) {
+                dto.setComputedPriority((float) (50.0 * adjustedPriorityFactor));
+            } else {
+                dto.setComputedPriority(1);
+            }
+        } else {
+            dto.setComputedPriority(1);
+        }
+    }
 }
